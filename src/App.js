fcbea3ab07af7285c617ab298cac88626ea4b92a
Niraj Kumar <niraj.hangingpanda@gmail.com>
2021-04-17 19:09:08 +0530

    created todo app using react redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import TodoForm from './Components/TodoForm'
+import AddTodo from './components/AddTodo/AddTodo'
+import List from './components/List/List'
 
 function App() {
   return (
-    <div>
-      <TodoForm />
-    </div>
+    < >
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="text-center">Todo App</h1>
+            </div>
+            <div className="col-md-12 mb-2 ">
+              <AddTodo />
+            </div>
+            <div className="col-md-12 border">
+              <h2 className="text-center">Todo Items</h2>
+              <hr className="w-50" />
+              <List />
+            </div>
+          </div>
+
+
+
+
+        </div>
+        <div className="col-md-3"></div>
+      </div>
+
+    </>
   )
 }
 
